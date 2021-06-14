fd7b5c22cbf182472154350a278714836ccd30b4
Lauren <TechnologicalType@MacBook-Air-2.local>
2021-06-14 14:54:19 -0400

    altered syntax

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 // import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Footer from './components/footer';
-import Header from './components/header';
-import {  Route, Switch } from 'react-router-dom';
-import Functional from './components/functional';
-import LifeCycle from './components/lifeCycles';
-import Axios from './components/axios';
-import Hooks from './components/hooks';
-import Landing from './components/landing';
+import './App.css'
+import React from 'react'
+import Footer from './components/footer'
+import Header from './components/header'
+import { Route, Switch } from 'react-router-dom'
+import Functional from './components/functional'
+import LifeCycle from './components/lifeCycles'
+import Axios from './components/axios'
+import Hooks from './components/hooks'
+import Landing from './components/landing'
 
-function App () {
+const App = () => {
   return (
-  
-  <>
-  <Header/>
-  
-  <Switch>
-  
-  <Route path='/life-cycles' component={LifeCycle} />          
-  <Route path='/functional' component={Functional} /> 
-  <Route path='/axios' component={Axios} />
-  <Route path='/hooks' component={Hooks}/>
-  <Route path='/' component={Landing}/>
+    <body>
+      <Header />
 
-  </Switch>
-  <Footer/>
-  </>
-
+      <Switch>
+        <Route path='/life-cycles' component={LifeCycle} />
+        <Route path='/functional' component={Functional} />
+        <Route path='/axios' component={Axios} />
+        <Route path='/hooks' component={Hooks} />
+        <Route path='/' component={Landing} />
+      </Switch>
+      <Footer />
+    </body>
   )
 }
 
-export default App;
+export default App
