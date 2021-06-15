e0b4c5fe34afd0022992d04ae0112b48b9dd44f1
Lauren <TechnologicalType@MacBook-Air-2.attlocal.net>
2021-06-15 14:13:04 -0400

    updated layout - tested api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react'
-import Footer from './components/footer/index.js'
+
 import Header from './components/header/index.js'
 import { Route, Switch } from 'react-router-dom'
 import Functional from './components/functional/index'
@@ -8,23 +8,24 @@ import LifeCycle from './components/lifeCycles/index'
 import Axios from './components/axios/index'
 import Hooks from './components/hooks/index'
 import Landing from './components/landing/index'
-
-
+import './App.css'
 const App = () => {
   return (
-    <main>
-      <Header />
+    <body>
+      <div className='content'>
+        <Header />
 
-      <Switch>
-        <Route path='/life-cycles' component={LifeCycle} />
-        <Route path='/functional' component={Functional} />
-        <Route path='/axios' component={Axios} />
-        <Route path='/hooks' component={Hooks} />
-        <Route path='/' component={Landing} />
-      </Switch>
-      <Footer />
-    </main>
+        <Switch>
+          <Route path='/life-cycles' component={LifeCycle} />
+          <Route path='/functional' component={Functional} />
+          <Route path='/axios' component={Axios} />
+          <Route path='/hooks' component={Hooks} />
+          <Route path='/' component={Landing} />
+        </Switch>
+      </div>
+      <footer className='footer'>Lauren &copy; 2021</footer>
+    </body>
   )
 }
 
-export default App
+export default App;
